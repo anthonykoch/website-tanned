0b152824437da0b9cc7c088308629eedf11b7833
Anthony Koch <hello@anthonykoch.com>
2026-01-17 17:01:26 -0700

    Move images and update imports

--- src/components/LaptopScroller2.tsx
+++ src/components/LaptopScroller2.tsx
@@ -1,7 +1,7 @@
-import ImageOpalcameraHomeFull1000w from '@root/public/actual/opalcamera-home-full-1000w.jpg'
-import ImageOpalcameraHomeFull1800w from '@root/public/actual/opalcamera-home-full-1800w.jpg'
-import ImageOpalcameraHomeFull750w from '@root/public/actual/opalcamera-home-full-750w.jpg'
-import ImageEmptyMacbook from '@root/public/final-compressed/empty-macbook.png'
+import ImageOpalcameraHomeFull1000w from '@/images/actual/opalcamera-home-full-1000w.jpg'
+import ImageOpalcameraHomeFull1800w from '@/images/actual/opalcamera-home-full-1800w.jpg'
+import ImageOpalcameraHomeFull750w from '@/images/actual/opalcamera-home-full-750w.jpg'
+import ImageEmptyMacbook from '@/images/final-compressed/empty-macbook.png'
 import {
   motion,
   MotionValue,
